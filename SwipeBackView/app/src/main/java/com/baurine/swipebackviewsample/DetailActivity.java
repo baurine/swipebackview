f072a41564fbf95f51ae7d01cbfeadcda2b54293
baurine <2008.hbl@gmail.com>
2017-02-16 19:08:31 +0800

    Implement sample with SwipeBackView

---
+++ SwipeBackView/app/src/main/java/com/baurine/swipebackviewsample/DetailActivity.java
@@ -0,0 +1,45 @@
+package com.baurine.swipebackviewsample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String KEY_BG_COLOR = "KEY_BG_COLOR";
+    private static final String KEY_ICON = "KEY_ICON";
+
+    private int bgColorResId;
+    private int iconResId;
+
+    public static void launch(Context from, int colorResId, int iconResId) {
+        Intent intent = new Intent(from, DetailActivity.class);
+        intent.putExtra(KEY_BG_COLOR, colorResId);
+        intent.putExtra(KEY_ICON, iconResId);
+        from.startActivity(intent);
+    }
+
+    //////////////////////////////////////////////////////////
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        getBundleData();
+        initViews();
+    }
+
+    private void getBundleData() {
+        bgColorResId = getIntent().getIntExtra(KEY_BG_COLOR, R.color.eggplant);
+        iconResId = getIntent().getIntExtra(KEY_ICON, R.drawable.icon_1);
+    }
+
+    private void initViews() {
+        Drawable drawable = getResources().getDrawable(bgColorResId);
+        findViewById(R.id.rl_root).setBackground(drawable);
+        ((ImageView) findViewById(R.id.iv_icon)).setImageResource(iconResId);
+    }
+}
--- SwipeBackView/app/src/main/java/com/baurine/swipebackviewsample/MainActivity.java
+++ SwipeBackView/app/src/main/java/com/baurine/swipebackviewsample/MainActivity.java
@@ -2,6 +2,7 @@ package com.baurine.swipebackviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        int bgColorResId = R.color.eggplant;
+        int iconResId = R.drawable.icon_1;
+        switch (view.getId()) {
+            case R.id.rl_icon_1:
+                bgColorResId = R.color.eggplant;
+                iconResId = R.drawable.icon_1;
+                break;
+            case R.id.rl_icon_2:
+                bgColorResId = R.color.saffron;
+                iconResId = R.drawable.icon_2;
+                break;
+            case R.id.rl_icon_3:
+                bgColorResId = R.color.sienna;
+                iconResId = R.drawable.icon_3;
+                break;
+        }
+        DetailActivity.launch(this, bgColorResId, iconResId);
     }
 }
