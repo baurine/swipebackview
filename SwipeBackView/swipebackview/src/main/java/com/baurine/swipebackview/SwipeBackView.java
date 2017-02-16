e604c352bbafa7005ae075a4c48440c32d00e91d
baurine <2008.hbl@gmail.com>
2017-02-16 18:38:12 +0800

    Implement SwipeBackView

---
+++ SwipeBackView/swipebackview/src/main/java/com/baurine/swipebackview/SwipeBackView.java
@@ -0,0 +1,90 @@
+package com.baurine.swipebackview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+public class SwipeBackView extends View {
+
+    public interface SwipeBackListener {
+        void onSwipeBack();
+    }
+
+    private SwipeBackListener swipeBackListener;
+
+    public void setSwipeBackListener(SwipeBackListener swipeBackListener) {
+        this.swipeBackListener = swipeBackListener;
+    }
+
+    ///////////////////////////////////////////
+    private View rootView;
+    private int touchSlop;
+    private Scroller scroller;
+
+    private boolean startScroll = false;
+    private float lastX;
+
+    public SwipeBackView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public SwipeBackView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        Activity activity = (Activity) context;
+        rootView = activity.getWindow().getDecorView();
+        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        scroller = new Scroller(context, new DecelerateInterpolator());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = event.getRawX();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float curX = event.getRawX();
+                int delta = (int) (curX - lastX);
+                if (delta > touchSlop && !startScroll) {
+                    startScroll = true;
+                }
+                if (startScroll && rootView.getScrollX() - delta <= 0) {
+                    rootView.scrollBy(-delta, 0);
+                    lastX = curX;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                startScroll = false;
+                if (-rootView.getScrollX() > rootView.getWidth() / 3) {
+                    if (swipeBackListener != null) {
+                        swipeBackListener.onSwipeBack();
+                    }
+                } else {
+                    scroller.startScroll(rootView.getScrollX(), 0, -rootView.getScrollX(), 0);
+                    invalidate();
+                }
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (scroller.computeScrollOffset()) {
+            rootView.scrollTo(scroller.getCurrX(), scroller.getCurrY());
+            invalidate();
+        }
+    }
+}
